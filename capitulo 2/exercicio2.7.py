28667d5affe2ece7fbd0e15d83c1b919350b8097
joaomarcosalves-dev <272327938+joaomarcosalves-dev@users.noreply.github.com>
2026-08-03 11:42:50 -0300

    Adicionando exercicio 2.7

---
+++ capitulo 2/exercicio2.7.py
@@ -0,0 +1,7 @@
+#Usando as propriedades da divisão e da multiplicação, tente entender como estes resultados são iguais: 0.2 * 6 + 8 * 0.3 + 7 * 0.5 = (20 * 6 + 8 * 30 + 7 * 50) / 100
+
+a = 0.2 * 6 + 8 * 0.3 + 7 * 0.5 
+b = (20 * 6 + 8 * 30 + 7 * 50) / 100
+
+if a == b:
+    print('Mesmo resultado')
