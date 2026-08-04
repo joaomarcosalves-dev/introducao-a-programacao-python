ec472d48a0f94bf62cd32b580243bdc3c7e40e7a
joaomarcosalves-dev <272327938+joaomarcosalves-dev@users.noreply.github.com>
2026-08-04 20:07:42 -0300

    Adicionando capitulo 3 e exercicios

---
+++ capitulo 3/exercicio3.1.py
@@ -0,0 +1,11 @@
+#Complete a tabela a seguir, marcando inteiro ou ponto flutuante dependendo do número apresentado.
+
+#int = inteiro
+#float = ponto flutuante
+
+5 #int
+5.0 #float
+4.3 #float
+-2 #int
+100 #int
+1.333 #float
