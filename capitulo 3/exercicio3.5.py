235060191a3328daacf2ec7eaa62f99296392cda
joaomarcosalves-dev <272327938+joaomarcosalves-dev@users.noreply.github.com>
2026-08-06 19:19:01 -0300

    Exercicio 3.5

---
+++ capitulo 3/exercicio3.5.py
@@ -0,0 +1,12 @@
+# Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.
+# A  B C     D      
+# 1  2 True  False
+# 10 3 False False
+# 5  1 True  True
+
+# A > B and C or D
+# 1 > 2(oq resulta False) and True(Resulta False,ja q o AND precisa de 2 condicoes verdadeiras para retonrar True) or False(Resultando False no final pq o or comparou False or False)
+
+# 10 > 3(da True) and False(Vai retornar False) or False(Retornara False como resultado)
+
+# 5 > 1(True) and True(Retorna True) or True(Retorna True no final)
