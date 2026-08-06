483c9af5362be322a21415790345e3e38dd695af
joaomarcosalves-dev <272327938+joaomarcosalves-dev@users.noreply.github.com>
2026-08-05 22:23:54 -0300

    Adicionando o exercico 3.2

---
+++ capitulo 3/exercicio3.2.py
@@ -0,0 +1,16 @@
+#Complete a tabela a seguir, respondendo True ou False. 
+#Considere a = 4, b = 10, c = 5.0, d = 1 e f = 5.
+
+a, b, c, d, f = 4, 10, 5.0, 1, 5
+
+a == c    #False   
+b > a     #True
+a < b     #True   
+c >= f    #True
+d > b     #False   
+f >= c    #True
+c != f    #False  
+c <= c    #True
+a == b    #False   
+c <= f    #True
+c < d     #False
