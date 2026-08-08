791dc4571a6b31628fb5456cc1856911b3f1ed2b
joaomarcosalves-dev <272327938+joaomarcosalves-dev@users.noreply.github.com>
2026-08-07 23:01:49 -0300

    Adicionando exercicio 3.6

---
+++ capitulo 3/exercicio3.6.py
@@ -0,0 +1,15 @@
+# Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. 
+# Para ser aprovado, todas as médias do aluno devem ser maiores que 7.
+# Considere que o aluno cursa apenas três matérias, e que a nota de cada uma está armazenada nas seguintes variáveis: matéria1, matéria2 e matéria3.
+
+#Seria materia1 > 7 and materia2 > 7 and materia3 > 7
+
+materia1 = 7
+materia2 = 8
+materia3 = 6
+
+if materia1 >= 7 and materia2 >= 7 and materia3 >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
+
